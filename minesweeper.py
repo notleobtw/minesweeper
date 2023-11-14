81435ee105bbdbfa83ed35c8f6c072fec7e5b76f
Leo <tha@wesleyan.edu>
2023-11-14 18:39:56 -0500

    skeleton code for the main game class

---
+++ minesweeper.py
@@ -0,0 +1,30 @@
+import pygame as pg
+
+class Game:
+    '''The main game class'''
+    def __init__(self) -> None:
+        pass
+
+    def set_difficulty(self, difficulty : str):
+        '''Set the game difficulty
+        
+        Args:
+            difficulty: the game difficulty
+        '''
+        pass
+
+    def new_game(self):
+        '''Generate a new game from the start'''
+        pass
+
+    def run_game(self):
+        '''Run the game'''
+        pass
+
+    def draw(self):
+        '''Display the game'''
+        pass
+
+    def close_game(self):
+        '''Closes the game'''
+        pass
